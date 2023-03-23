412b309027a4e805260cb02cd3d5b9fa97ca5e1a
Mustafa Afandi <tapaafandi@gmail.com>
2023-03-23 15:07:34 +0700

    changing the string value of "saved"

--- app/src/main/java/com/tapaafandi/dicoplay/domain/model/MinSpec.kt
+++
@@ -1,9 +0,0 @@
-package com.tapaafandi.dicoplay.domain.model
-
-data class MinSpec(
-    val graphics: String,
-    val memory: String,
-    val os: String,
-    val processor: String,
-    val storage: String
-)
--- app/src/main/java/com/tapaafandi/dicoplay/domain/model/Screenshot.kt
+++
@@ -1,6 +0,0 @@
-package com.tapaafandi.dicoplay.domain.model
-
-data class Screenshot(
-    val id: Int,
-    val image: String
-)
