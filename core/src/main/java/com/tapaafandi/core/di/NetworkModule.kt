4d69b2bb723512a53b8f6693dadb95e0890a5de1
Mustafa Afandi <tapaafandi@gmail.com>
2023-03-24 23:28:27 +0700

    implementing certificate pinning with okhttp

--- core/src/main/java/com/tapaafandi/core/di/NetworkModule.kt
+++ core/src/main/java/com/tapaafandi/core/di/NetworkModule.kt
@@ -3,10 +3,12 @@ package com.tapaafandi.core.di
 import com.tapaafandi.core.BuildConfig
 import com.tapaafandi.core.data.network.retrofit.RetrofitDicoplayNetworkApi
 import com.tapaafandi.core.uitls.Constants.BASE_URL
+import com.tapaafandi.core.uitls.Constants.HOST_NAME
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -17,10 +19,16 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
-
     @Provides
     @Singleton
     fun providesHttpClient(): OkHttpClient {
+
+        val certificatePinner = CertificatePinner.Builder()
+            .add(HOST_NAME, "sha256/aibC8oPHcGrFgJi2WmuIncc4TkOr+XPZfupJR+2yZ9g=")
+            .add(HOST_NAME, "sha256/8kGWrpQHhmc0jwLo43RYo6bmqtHgsNxhARjM5yFCe/w=")
+            .add(HOST_NAME, "sha256/gI1os/q0iEpflxrOfRBVDXqVoWN3Tz7Dav/7IT++THQ=")
+            .build()
+
         return OkHttpClient.Builder()
             .addInterceptor(
                 HttpLoggingInterceptor()
@@ -30,6 +38,7 @@ object NetworkModule {
                         }
                     }
             )
+            .certificatePinner(certificatePinner)
             .readTimeout(15, TimeUnit.SECONDS)
             .connectTimeout(15, TimeUnit.SECONDS)
             .build()
--- core/src/main/java/com/tapaafandi/core/uitls/Constants.kt
+++ core/src/main/java/com/tapaafandi/core/uitls/Constants.kt
@@ -2,6 +2,8 @@ package com.tapaafandi.core.uitls
 
 object Constants {
 
+    const val HOST_NAME = "www.freetogame.com"
+
     const val BASE_URL = "https://www.freetogame.com/api/"
 
 }
